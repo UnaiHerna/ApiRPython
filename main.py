3e283c241c2ef4c04f0d907bb7ff227480276a5f
UnaiHernandezIzcue <uhernandez@cimico.tech>
2024-07-22 09:08:02 +0200

    Uvicorn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import rpy2.robjects as robjects
+import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -100,3 +101,13 @@ async def heatmap(db: Session = Depends(get_db)):
 
     set_cached_response(cache_key, datos_dict)
     return datos_dict
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=443,
+        ssl_keyfile="/etc/letsencrypt/live/tu_dominio.com/privkey.pem",
+        ssl_certfile="/etc/letsencrypt/live/tu_dominio.com/fullchain.pem"
+    )
